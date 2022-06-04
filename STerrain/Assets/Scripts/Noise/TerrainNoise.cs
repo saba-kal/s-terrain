88de2c55741f72bd55ab53b17b0fd839fc0fcd0d
saba-kal <38592380+saba-kal@users.noreply.github.com>
2022-06-03 23:03:07 -0400

    Created terrain shader.

--- STerrain/Assets/Scripts/Noise/TerrainNoise.cs
+++ STerrain/Assets/Scripts/Noise/TerrainNoise.cs
@@ -1,4 +1,5 @@
 ﻿using STerrain.EndlessTerrain;
+using Unity.Collections;
 using UnityEngine;
 
 namespace STerrain.Noise
@@ -51,6 +52,52 @@ namespace STerrain.Noise
             }
 
             return voxelData;
+        }
+
+        /// <summary>
+        /// Generates a 3D noise map.
+        /// </summary>
+        public static void Generate(
+            int mapWidth,
+            int mapHeight,
+            int mapDepth,
+            int scale,
+            Vector3 sampleCenter,
+            NoiseSettings noiseSettings,
+            int nativeArrayStartIndex,
+            NativeArray<float> voxelData)
+        {
+            var offsetX = noiseSettings.Offset.x + sampleCenter.x;
+            var offsetY = noiseSettings.Offset.y + sampleCenter.y;
+            var offsetZ = noiseSettings.Offset.z + sampleCenter.z;
+
+            var fastNoise = new FastNoiseLite(noiseSettings.Seed);
+            fastNoise.SetFractalType(FastNoiseLite.FractalType.FBm);
+            fastNoise.SetFractalOctaves(noiseSettings.Octaves);
+            fastNoise.SetFractalLacunarity(noiseSettings.Lacunarity);
+            fastNoise.SetFractalGain(noiseSettings.Persistence);
+            fastNoise.SetNoiseType(FastNoiseLite.NoiseType.OpenSimplex2);
+
+            for (var x = 0; x < mapWidth; x++)
+            {
+                for (var y = 0; y < mapHeight; y++)
+                {
+                    for (var z = 0; z < mapDepth; z++)
+                    {
+                        var noiseLocation = new Vector3(
+                            (x + offsetX) * scale * noiseSettings.Scale,
+                            (y + offsetY) * scale * noiseSettings.Scale,
+                            (z + offsetZ) * scale * noiseSettings.Scale);
+
+                        var noiseValue = fastNoise.GetNoise(
+                            noiseLocation.x,
+                            noiseLocation.y * 2,
+                            noiseLocation.z);
+
+                        voxelData[nativeArrayStartIndex + (z * mapWidth * mapHeight) + (y * mapWidth) + x] = noiseValue + (noiseLocation.y / 50f);
+                    }
+                }
+            }
         }
     }
 }
